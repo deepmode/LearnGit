e42595293e57a496c4af29e018c0ac12836fd603
Eric Ho <eric.ho@101medialab.com>
2024-05-22 14:31:50 +0800

    feat: added pink color

--- LearnGit/ViewController.swift
+++ LearnGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Welcome to WWDC"
+        view.backgroundColor = UIColor.systemPink
     }
 
 
