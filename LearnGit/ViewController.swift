6bab200023e0161ab761e2fc1f44bce14ecd81de
Eric Ho <eric.ho@101medialab.com>
2024-05-22 21:30:05 +0800

    feat: Update view background color to green and add border with color red

--- LearnGit/ViewController.swift
+++ LearnGit/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Welcome to Google I/O 2024"
-        view.backgroundColor = UIColor.orange
+        view.backgroundColor = UIColor.green
+        view.layer.borderColor = UIColor.red.cgColor
+        view.layer.borderWidth = 10.0
     }
 }
 
