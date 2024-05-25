a77abb512d7ce7434a90a4f437c748c4d988230e
Eric Ho <eric.ho@101medialab.com>
2024-05-25 20:31:47 +0800

    feat: Added ComscoreAnalytics, fix the log message errror  in PermutiveAnalyticsClient

---
+++ LearnGit/Analytics/ComsocreAnalyticsClient.swift
@@ -0,0 +1,19 @@
+//
+//  ComsocreAnalyticsClient.swift
+//  LearnGit
+//
+//  Created by Eric Ho on 5/25/24.
+//
+
+import Foundation
+
+class ComsocreAnalyticsClient: AnalyticsProtocol {
+    
+    init() {
+        
+    }
+    
+    func sendEvent(name: String, parameters: [String : String]) {
+        print("\(type(of: self)): \(#function): \(name) : \(parameters)")
+    }
+}
