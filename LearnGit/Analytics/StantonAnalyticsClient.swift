1a9bb2dbd2af18881b096274109a2c5c43432121
Eric Ho <eric.ho@101medialab.com>
2024-05-26 08:45:04 +0800

    feat: Added StantonAnalyticsClient

---
+++ LearnGit/Analytics/StantonAnalyticsClient.swift
@@ -0,0 +1,23 @@
+//
+//  StantonAnalytics.swift
+//  LearnGit
+//
+//  Created by Eric Ho on 5/26/24.
+//
+
+import Foundation
+
+class StantonAnalyticsClient: AnalyticsProtocol {
+    
+    init() {
+        config()
+    }
+    
+    func config() {
+        
+    }
+    
+    func sendEvent(name: String, parameters: [String : String]) {
+        print("\(type(of: self)): \(#function): \(name) : \(parameters)")
+    }
+}
