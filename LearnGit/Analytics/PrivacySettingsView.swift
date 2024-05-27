0f2d650a763396a2f3ff762b89141796e95d272a
Eric Ho <eric.ho@101medialab.com>
2024-05-27 12:25:24 +0800

    feat: added PrivacySettingsView

---
+++ LearnGit/Analytics/PrivacySettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  PrivacySettingsView.swift
+//  LearnGit
+//
+//  Created by Eric Ho on 5/27/24.
+//
+
+import SwiftUI
+
+struct PrivacySettingsView: View {
+    var body: some View {
+        Text("Privacy Settings")
+    }
+}
+
+#Preview {
+    PrivacySettingsView()
+}
