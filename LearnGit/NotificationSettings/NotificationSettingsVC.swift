d8a9843b279b5c93509f3a54162d781dfef26a70
Eric Ho <eric.ho@101medialab.com>
2025-05-24 16:39:27 +0800

    [Clean]: added my Git rule for the project.

---
+++ LearnGit/NotificationSettings/NotificationSettingsVC.swift
@@ -0,0 +1,30 @@
+//
+//  NotificationSettings.swift
+//  LearnGit
+//
+//  Created by Eric Ho on 5/24/25.
+//
+
+import UIKit
+
+class NotificationSettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Notification Settings"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
